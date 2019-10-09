701b211e2d3a55996f417ba703fdce164056f2f2
walkerl <linwal@bluewin.ch>
2019-10-09 13:13:41 +0200

    first setup of import functions from Entso-E, still a problem with annual total production and units

--- power_data_collection.py
+++ power_data_collection.py
@@ -1,11 +1,11 @@
 import pandas as pd
-import entsoe as ee
+from entsoe import EntsoePandasClient
 
 
 ## Entsoe Transparancy platform login:
 
-key = "285a3e21-c574-453b-9690-e327c7bd73fd "  ## don't publish
-
+key = "285a3e21-c574-453b-9690-e327c7bd73fd"  ## don't publish
+client = EntsoePandasClient(api_key=key)
 
 ## Time window:
 
@@ -15,4 +15,21 @@ end_time = pd.Timestamp('20151231', tz='Europe/Brussels')
 country_code = "CH" # Switzerland
 
 neighbour_countries = ["AT", "DE", "FR", "IT"]
+
+
+domestic_production = client.query_generation(country_code, start=start_time, end=end_time, psr_type=None)
+print(domestic_production.sum(axis=0).sum())
+
+exit()
+neighbour_production = []
+
+for country in neighbour_countries:
+    neighbour_production.append(client.query_generation(country_code, start=start_time, end=end_time, psr_type=None))
+
+print("Vogel")
+
+
+cross_border_flows = []
+for country in neighbour_countries:
+    cross_border_flows.append(client.query_crossborder_flows(country, country_code, start=start_time, end=end_time))
 
