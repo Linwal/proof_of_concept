b6889d8f79fbef8d5ebf1cc70521842e6cc1925b
walkerl <linwal@bluewin.ch>
2019-10-10 09:47:03 +0200

    bugfix: resampling of imported data is changed because it is imported in power (MW) and not Energy (MWh). Therefore data with higher resolution than 1 hour is now resampled according to mean than sum.

--- power_data_collection.py
+++ power_data_collection.py
@@ -1,5 +1,6 @@
 import pandas as pd
 from entsoe import EntsoePandasClient
+import matplotlib.pyplot as plt
 
 
 ## Entsoe Transparancy platform login:
@@ -9,20 +10,20 @@ client = EntsoePandasClient(api_key=key)
 
 ## Time window:
 
-start_time = pd.Timestamp('20150101', tz='Europe/Brussels')
-end_time = pd.Timestamp('20150130', tz='Europe/Brussels')
+start_time = pd.Timestamp('20160101', tz='Europe/Brussels')
+end_time = pd.Timestamp('20161231', tz='Europe/Brussels')
 
 country_code = "CH" # Switzerland
 
 neighbour_countries = ["AT", "DE", "FR", "IT"]
 
-
-domestic_production = client.query_generation(country_code, start=start_time, end=end_time, psr_type=None)
-
+# it is resampled to an hour with mean because the values coming out of the database are acutally given in MW and not in MWh
+domestic_production = client.query_generation(country_code, start=start_time, end=end_time, psr_type=None).resample("H").mean()
 neighbour_production = []
 
 for country in neighbour_countries:
-    neighbour_production.append(client.query_generation(country, start=start_time, end=end_time, psr_type=None))
+    neighbour_production.append(client.query_generation(country, start=start_time, end=end_time, psr_type=None).resample("H").mean())
+
 
 emission_production_matrix_filepath = r"C:\Users\walkerl\Documents\code\proof_of_concept\data\emissions_technology_matrix.xlsx"
 emission_production_matrix = pd.read_excel(emission_production_matrix_filepath, index_col=0)
@@ -53,11 +54,38 @@ for neighbour in neighbour_production:
     neighbour_emission_factors.append(neighbour_emissions_df.sum(axis=1).divide(neighbour.sum(axis=1)))
 
 
-quit()
-
-
-cross_border_flows = []
+import_cross_border_flows = [] # if the flows are power and not energy, change the resampling from sum to mean
 for country in neighbour_countries:
-    cross_border_flows.append(client.query_crossborder_flows(country, country_code, start=start_time, end=end_time))
+    # resampling to the hour with mean because quarter hourly values are given in MW not MWh
+    import_cross_border_flows.append(client.query_crossborder_flows(country, country_code, start=start_time, end=end_time).resample("H").mean())
 
 
+
+#Initialization of tatal factor with total summation of emmissions of domestic production
+total_emission_factor = domestic_emissions_df.sum(axis=1)
+# print("Gelb")
+# print(total_emission_factor)
+# print("hier_gopfer")
+# print(import_cross_border_flows[0])
+# print("am gopfsten")
+# print(neighbour_emission_factors[0])
+# print("hier_gopf")
+# print(import_cross_border_flows[0]*neighbour_emission_factors[0])
+# print("hier werden die nefs geprinted:")
+# print(neighbour_emission_factors)
+# quit()
+
+
+total_production_and_import = domestic_production.sum(axis=1)
+
+for i in range(len(neighbour_countries)):
+    # fill_value=0 resolves the problem of having NANs in the cross_border_flows
+    total_emission_factor = total_emission_factor +(import_cross_border_flows[i] * (neighbour_emission_factors[i]))
+    total_production_and_import.add(import_cross_border_flows[i])
+
+total_emission_factor = total_emission_factor.divide(total_production_and_import)
+print(total_emission_factor)
+
+
+plt.plot(total_emission_factor)
+plt.show()
